8413eca1e962e444f11861ea892f453ca7d952bd
Sway-Tree <agnieszka.dudek92@gmail.com>
2013-10-20 17:09:10 +0000

    Open: 7am - 18pm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shop::Application.config.secret_key_base = '0fd66f2090e9e8bad7120ac777f52112e0443804982f12deac578b1b0121686a8fa6cda66f30c266d52c6cd42a8c533c491e4970d93281b9bf66d6930d090c12'
